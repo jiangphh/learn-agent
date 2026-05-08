41f25556d4270811678124463aeeab49a2536642
jjjjph <18283190712@163.com>
2026-05-08 11:28:48 +0800

    feat: heartbeat api

--- travel-server/src/index.js
+++ travel-server/src/index.js
@@ -3,6 +3,16 @@ import express from 'express'
 const app = express()
 
 const port = 3222
+
+// // 创建一个心跳接口
+// app.get('/heartbeat',(req,res)=>{
+//     // req请求头，res响应
+//     res.json({
+//         message:'请求成功',
+//         time:new Date().toISOString()
+//     })
+// })
+
 app.listen(port,()=>{
     console.log(`server port:http://localhost:${port}`)
 })
